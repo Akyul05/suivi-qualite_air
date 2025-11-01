de46a94a8b38726ec42ceac11b10913387cee315
Akyul05 <akyulramy05@gmail.com>
2025-11-01 23:19:06 +0100

    git push origin feature/layout-statique

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; // On importe notre fichier CSS
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        <h1>Suivi de la Qualité de l'Air</h1>
       </header>
+
+      <main>
+        <div className="search-container">
+          <input type="text" placeholder="Rechercher une ville..." />
+          <button>Rechercher</button>
+        </div>
+
+        <div className="results-container">
+          <h2>Résultats pour : Paris</h2>
+
+          {/* On simule une alerte "orange". 
+            Le "className" nous servira pour le style.
+          */}
+          <div className="card-resultat card-orange"> 
+            <p>Indice de l'air (AQI)</p>
+            <span className="aqi-valeur">78</span>
+            <p className="aqi-niveau">Modéré</p>
+            <p className="aqi-reco">Recommandation : Les groupes sensibles doivent être prudents.</p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
